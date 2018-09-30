23dc5c04a0dd8349f7724b8d89d5deb9c1a2d83f
Joe Begley <joebegley15@gmail.com>
2018-09-30 16:01:22 -0400

    Bug fix

--- django_start/urls.py
+++ django_start/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import	staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin', admin.site.urls),
-    url(r'^articles', include('articles.urls')),
+    url(r'^articles/', include('articles.urls')),
     url(r'^about', views.about),
     url(r'^$', views.home)
 ]
